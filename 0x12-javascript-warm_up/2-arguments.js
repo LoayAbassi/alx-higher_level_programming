c8f1415f3351a6129af98601c9893902d4d9c214
LoayAbassi <abassi.loay@gmail.com>
2024-07-01 03:15:02 +0100

    ya rabi

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
+/* this file counts arguments from terminal and returns number approximately */
 const args = process.argv.slice(2);
-if (args.length == 0){
-    console.log('No argument');
-}
-else if (args.length == 1){
-    console.log('Argument found');
-}
-else{
-    console.log('Arguments found');
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
