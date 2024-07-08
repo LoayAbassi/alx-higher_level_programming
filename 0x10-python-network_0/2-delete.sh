371dd96227598c08872f798845e63bff7f3f3b5e
LoayAbassi <87607081+LoayAbassi@users.noreply.github.com>
2024-07-08 02:55:07 +0100

    Update 2-delete.sh

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-<<<<<<< HEAD
-#displays body of response
-curl -sL "$1"
-
-=======
 #sending a delete request and printing the response
 curl -sL -X DELETE $1
->>>>>>> f8c731af94b140db6c896fa3ef55e78bb97a7d02
