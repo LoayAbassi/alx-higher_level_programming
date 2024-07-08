0b12c6717d7ee866fc667a9b3366be99cf1ebc09
LoayAbassi <87607081+LoayAbassi@users.noreply.github.com>
2024-07-08 02:56:46 +0100

    Update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #this will show the list of possible methods a url can accept
 curl -sL -i $1 | grep -i "Allow:" |cut -d ' ' -f2-
-<<<<<<< HEAD
-
-=======
->>>>>>> f8c731af94b140db6c896fa3ef55e78bb97a7d02
