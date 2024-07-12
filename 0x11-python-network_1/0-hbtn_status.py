04d494787fd349eef1b4d7f59ff31744dbc79900
LoayAbassi <abassi.loay@gmail.com>
2024-07-12 19:35:34 +0100

    hhhh

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,10 +9,10 @@ if __name__== "__main__" :
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         body = response.read()
-        print("Body response:")
-        print("\t- type:",type(body) )
-        print("\t- content:",body)
-        print("\t- utf8 content:",body.decode("utf-8"))
+        print("Body response :")
+        print("\t- type :",type(body) )
+        print("\t- content :",body)
+        print("\t- utf8 content :",body.decode("utf-8"))
 
 
 
