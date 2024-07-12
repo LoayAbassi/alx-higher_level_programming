38e081402e18016971a89fbdc341e3358362d495
LoayAbassi <abassi.loay@gmail.com>
2024-07-12 19:41:46 +0100

    agggggg

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,17 +2,14 @@
 """fetches the give url"""
 import urllib.request
 
-if __name__== "__main__" : 
+if __name__ == "__main__":
 
     url = "https://alx-intranet.hbtn.io/status"
 
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         body = response.read()
-        print("Body response :")
-        print("\t- type :",type(body) )
-        print("\t- content :",body)
-        print("\t- utf8 content :",body.decode("utf-8"))
-
-
-
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", body.decode("utf-8"))
