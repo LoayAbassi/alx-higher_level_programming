2bbb9f145f418ce3f94c0e7839aef2d0d9b1e4af
LoayAbassi <abassi.loay@gmail.com>
2024-07-15 14:20:53 +0100

    yooooh

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,4 +3,9 @@
 import urllib.request
 import sys
 if __name__ == "__main__" : 
-    #url for argument https://alx-intranet.hbtn.io 
+    url = "https://alx-intranet.hbtn.io"
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response : 
+        print(response.headers["X-Request-Id"])
+
+
