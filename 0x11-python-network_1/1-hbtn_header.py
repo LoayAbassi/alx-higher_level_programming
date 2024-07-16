2e54ceb64e7ccf39f5292a19371aa00b49096fab
LoayAbassi <abassi.loay@gmail.com>
2024-07-16 01:51:07 +0100

    maybe

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,8 +2,7 @@
 """sends request to URL => displays value of X-Request-Id variable found in the header of the response."""
 import urllib.request
 import sys
-if __name__ == "__main__" : 
-    url = "https://alx-intranet.hbtn.io"
-    request = urllib.request.Request(url)
-    with urllib.request.urlopen(request) as response : 
-        print(response.headers["X-Request-Id"])
+url = sys.argv[1]
+request = urllib.request.Request(url)
+with urllib.request.urlopen(request) as response : 
+    print(response.headers["X-Request-Id"])
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+"""Sends a request, shows result in body of response (handeling exceptions )"""
+
+if __name__ == "__main__ "
