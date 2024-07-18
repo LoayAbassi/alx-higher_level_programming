79243627e7663758affc9d9752a682e2b05707c3
LoayAbassi <abassi.loay@gmail.com>
2024-07-18 12:52:53 +0100

    pycodstyle fixes

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.
+"""sends a POST request to 
+http://0.0.0.0:5000/search_user with the letter as a parameter.
 
 The letter must be sent in the variable q
 If no argument is given, set q=""
-If the response body is properly JSON formatted and not empty, display the id and name like this: [<id>] <name>
+If the response body is properly JSON formatted 
+and not empty, display the id and name like this: [<id>] <name>
 Otherwise:
 Display Not a valid JSON if the JSON is invalid
 Display No result if the JSON is empty"""
