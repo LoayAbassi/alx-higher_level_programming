96259ae853f8e9bffff791f6887240ada25ed5df
LoayAbassi <abassi.loay@gmail.com>
2024-07-18 12:53:48 +0100

    pycodestyle

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""sends a POST request to 
+"""sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
-
 The letter must be sent in the variable q
 If no argument is given, set q=""
-If the response body is properly JSON formatted 
+If the response body is properly JSON formatted
 and not empty, display the id and name like this: [<id>] <name>
 Otherwise:
 Display Not a valid JSON if the JSON is invalid
@@ -18,7 +17,7 @@ if __name__ == "__main__":
     else:
         q = sys.argv[1]
     url = "http://0.0.0.0:5000/search_user"
-    
+
     try:
         r = requests.post(url, data={"q": q})
         r_json = r.json()
