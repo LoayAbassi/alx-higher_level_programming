8746430cc06d109b09e2f9d2745625edd3b995da
LoayAbassi <abassi.loay@gmail.com>
2024-07-21 17:17:53 +0100

    type changed

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    print(f"{str(number)[-1]}", end="")
+    print(f"{int(str(number)[-1])}", end="")
     return int(str(number)[-1])
+print_last_digit("Holberton")
