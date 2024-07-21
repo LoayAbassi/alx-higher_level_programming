1fe303c8d771c40cb29e7128d3346464ad833508
LoayAbassi <abassi.loay@gmail.com>
2024-07-21 17:32:06 +0100

    frmt err

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,4 @@ elif (x == 0):
     print("Last digit of {:d} is {:d} and is 0".format(number, x))
 else:
     print(
-        "Last digit of {:d} is {:d} and is less than 6 \
-            and not 0".format(number, x))
+        "Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, x))
