9919414a20a96291a6f140fa5380a8b628b410a8
LoayAbassi <abassi.loay@gmail.com>
2024-07-21 17:27:56 +0100

    arithmetics err

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 x = int(str(number)[-1])
+if number < 0:
+    x = -x
 if (x > 5):
     print("Last digit of {:d} is {:d} and is greater than 5".format(number, x))
 elif (x == 0):
     print("Last digit of {:d} is {:d} and is 0".format(number, x))
 else:
-    print("Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, x))
+    print(
+        "Last digit of {:d} is {:d} and is less than 6 and not 0".format(number, x))
