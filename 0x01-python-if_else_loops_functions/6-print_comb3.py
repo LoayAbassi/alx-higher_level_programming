b425a15b873985f129df3a0051a480d35c22922c
LoayAbassi <abassi.loay@gmail.com>
2024-07-22 14:40:48 +0100

    yoo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 for i in range(100):
-    if (i % 10 > i//10):
-        print("{:02},".format(i), end=" ")
+    if(i==1):
+        print("{:02}".format(i), end="")
+    if (i % 10 > i//10 and i!=1):
+        print(", {:02}".format(i), end="")
     if (i == 99):
         print()
+    
