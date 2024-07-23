8670992717cea3341237fac4ee72216353ea05a9
LoayAbassi <abassi.loay@gmail.com>
2024-07-23 18:56:04 +0100

    dddd

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,6 +4,6 @@ def safe_print_list(my_list=[], x=0):
     try:
         for i in range(x):
             print("{}".format(my_list[i]), end="")
-        print()
+        return x
     except:
         pass
