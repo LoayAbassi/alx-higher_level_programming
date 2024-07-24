3112047a3982ac7bec6abcf97fd4d3dd31fd7ad8
LoayAbassi <abassi.loay@gmail.com>
2024-07-24 13:29:00 +0100

    pycodestyle fixs

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,12 +4,11 @@ def safe_print_list_integers(my_list=[], x=0):
         nb = 0
         for i in range(x):
             try:
-                print("{:d}".format(),end = "")
-                nb+=1
+                print("{:d}".format(), end="")
+                nb += 1
             except:
                 continue
     except:
         pass
 
     return nb
-
