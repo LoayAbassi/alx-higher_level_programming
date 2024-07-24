74566e6aa0f28f82e1f888736a265a0747b04fed
LoayAbassi <abassi.loay@gmail.com>
2024-07-24 13:35:52 +0100

    err fxd

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    """Prints x elements of a list."""
-    try:
-        for i in range(x):
+    nb = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
-        print()
-        return x
-    except:
-        pass
+            nb += 1
+        except IndexError:
+            break
+    print()
+    return nb
+
+
+my_list = [1, 2, 3, 4]
+x = len(my_list) + 1
+
+nb_print = safe_print_list(my_list, x)
+print("{:d}".format(nb_print))
