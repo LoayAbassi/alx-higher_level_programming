268b3e1c87f51382d0e4554fe37060a2e7ce978a
LoayAbassi <abassi.loay@gmail.com>
2024-07-27 15:09:07 +0100

    maybe

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -119,7 +119,7 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        elif not (isinstance(value[0], int) and isinstance(value[1], int)):
+        elif not isinstance(value[0], int) or not isinstance(value[1], int):
             raise TypeError("position must be a tuple of 2 positive integers")
         elif value[0] < 0 or value[1] < 0:
             raise TypeError("position must be a tuple of 2 positive integers")
