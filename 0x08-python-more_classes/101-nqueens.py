08c4dc0a23ccba420b10b95b9bd4ad273a4c742a
LoayAbassi <abassi.loay@gmail.com>
2024-07-29 16:12:14 +0100

    fixed

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+"""101-nqueens.py
+This module contains a function that solves the N-Queens problem.
+"""
+import sys
+n = int(sys.argv[1])
--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -170,6 +170,7 @@ class Rectangle:
             return (rect_1)
         return rect_2
 
+    @classmethod
     def square(cls, size=0):
         """
         width == height == size
