aadd9366654f756ca487e41fd02fbb36bfbb4d30
LoayAbassi <abassi.loay@gmail.com>
2024-07-30 11:42:21 +0100

    fxd

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -10,11 +10,12 @@ def add_integer(a, b=98):
     Returns:
         int: The sum of a and b.
     """
-    if not isinstance(a,(int,float)):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if not isinstance(b,(int,float)):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return a+b
+    return int(a)+int(b)
+
 
 if __name__ == "__main__":
     import doctest
