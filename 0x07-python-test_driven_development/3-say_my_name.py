952929b220258fc6a42d3922122ee17c717f5da6
LoayAbassi <abassi.loay@gmail.com>
2024-07-30 12:59:33 +0100

    added 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+3-say_my_name.py
+this file contains a function that print full name
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints out my full name
+    Args:
+        first_name (str): the first name
+        last_name (str): the last name (default is an empty string)
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print(f"My name is {first_name} {last_name}")
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
