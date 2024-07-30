165b659b0a3246551e6a39f559be00ae219342cb
LoayAbassi <abassi.loay@gmail.com>
2024-07-30 13:17:42 +0100

    fixed print message

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+"""Module containing a method that adds 2 new lines"""
+
+
+def text_indentation(text):
+    """Method for adding 2 new lines after '.?:' chars.
+
+    Args:
+        text: The str text.
+
+    Raises:
+        TypeError: If text is not a str.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delim in ".?:":
+        text = (delim + "\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print(text, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
