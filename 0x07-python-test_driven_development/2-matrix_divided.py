f1aea189b271cca20879dd3c8649af5bebdd78c2
LoayAbassi <abassi.loay@gmail.com>
2024-07-30 13:19:10 +0100

    some fixes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,6 +3,8 @@
     module contains a function that devides a matrix's elements 
     after checking for some specific conditions.
     """
+
+
 def matrix_divided(matrix, div):
     """Divide each element of a matrix by a given number.
     Args:
@@ -11,27 +13,31 @@ def matrix_divided(matrix, div):
     Returns:
     list: A new 2D list with each element divided by the given number.
     """
-    
-    if not isinstance (div,(int,float)):
+
+    if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if not isinstance(matrix,list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not isinstance(matrix, list):
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats")
     for i in matrix:
-        if not( isinstance(i,list) and len(i)!=0):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        if not (isinstance(i, list) and len(i) != 0):
+            raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats")
         if len(i) != len(matrix[0]):
             raise TypeError("Each row of the matrix must have the same size")
-        for e in i : 
-            if not isinstance(e,(int,float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-            
+        for e in i:
+            if not isinstance(e, (int, float)):
+                raise TypeError(
+                    "matrix must be a matrix (list of lists) of integers/floats")
+
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            matrix[i][j] = round(matrix[i][j] / div,2)
-            
+            matrix[i][j] = round(matrix[i][j] / div, 2)
+
     return matrix
+
 
 """if __name__ =="__main__":
     import doctest
