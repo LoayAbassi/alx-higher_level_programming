f8bc6d0cdda18c42169885300f57dea9fa7c1720
LoayAbassi <abassi.loay@gmail.com>
2024-08-01 14:19:52 +0100

    fixed 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,16 +4,20 @@ const url = process.argv[2];
 const request = require('request');
 
 request(url, (err, response, body) => {
-  const unbtc = {};
-  const tasks = JSON.parse(body);
-  for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i].completed === true) {
-      if (unbtc.hasOwnProperty(tasks[i].userId)) {
-        unbtc[tasks[i].userId] += 1;
-      } else {
-        unbtc[tasks[i].userId] = 1;
+  if (err) {
+    console.log(err);
+  } else {
+    const unbtc = {};
+    const tasks = JSON.parse(body);
+    for (let i = 0; i < tasks.length; i++) {
+      if (tasks[i].completed === true) {
+        if (tasks[i].userId in unbtc) {
+          unbtc[tasks[i].userId] += 1;
+        } else {
+          unbtc[tasks[i].userId] = 1;
+        }
       }
     }
+    console.log(unbtc);
   }
-  console.log(unbtc);
 });
