5e93d81e7b4326c3c6379e392d32881858f25812
LoayAbassi <abassi.loay@gmail.com>
2024-08-02 18:57:56 +0100

    fixes

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists record of names and scores(base of order)
---without counting names with no value
+-- without counting names with no value
 SELECT score, name
 FROM second_table
 WHERE name != ""
