252175b3800a1c25d188f76e6dd819b3327a9f3b
LoayAbassi <abassi.loay@gmail.com>
2024-08-03 17:56:50 +0100

    SSSS

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- lists genres and number of shows related to each one
-SELECT tv_genres.name as genre, count(tv_show_genres.show_id)
-as number_of_shows
+SELECT tv_genres.name AS genre, count(tv_show_genres.show_id)
+AS number_of_shows
 FROM tv_genres,tv_show_genres 
 WHERE tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
