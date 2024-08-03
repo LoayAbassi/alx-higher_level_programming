fb3748b1420cfbf720c5f2b236a3def9013e034e
LoayAbassi <abassi.loay@gmail.com>
2024-08-03 15:49:22 +0100

    finished

---
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,8 @@
+--lists all cities of calefornia
+SELECT id, name
+FROM cities
+WHERE state_id = (
+      SELECT id
+      FROM states
+      WHERE name = 'California'
+);
---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,5 @@
+-- lists cities.id - cities.name - states.name 
+SELECT cities.id, cities.name, states.name
+FROM cities, states
+WHERE cities.state_id = states.id
+ORDER BY cities.id;
