77dd6c8a077f7bb424cc572ddeda096627005244
LoayAbassi <abassi.loay@gmail.com>
2024-08-03 18:41:51 +0100

    more work

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- lists all comedy shows 
+SELECT tv_shows.title 
+FROM tv_shows,tv_genres,tv_show_genres
+WHERE 
+tv_shows.id = tv_show_genres.show_id
+AND
+tv_show_genres.genre_id = tv_genres.id
+AND tv_genres.name = "Comedy"
+ORDER BY tv_shows.title ASC;
